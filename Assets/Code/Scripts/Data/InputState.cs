98a74eb2b085827121e7d229545b05cfac4e8217
Georgiy Ryaposov <sxwx87@gmail.com>
2025-05-15 14:46:18 +0700

    Added ThirdPersonController, test stage scene, etc. minor things

---
+++ Assets/Code/Scripts/Data/InputState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.Data
+{
+    public class InputState
+    {
+        public Vector2 Move;
+        public Vector2 Look;
+        public bool Sprint;
+        public bool Jump;
+        
+        public bool IsCurrentDeviceMouse = true;
+        public bool AnalogMovement;
+    }
+}
